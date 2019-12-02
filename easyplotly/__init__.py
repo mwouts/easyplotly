8e0c40f663de7e56ee6b9da3b3e1db2bddb7e83d
Marc Wouts <marc.wouts@gmail.com>
2019-12-02 01:07:01 +0100

    Add __version__

--- easyplotly/__init__.py
+++ easyplotly/__init__.py
@@ -4,5 +4,6 @@ import plotly.graph_objects as go
 from .sankey import Sankey
 from .sunburst import Sunburst
 from .treemap import Treemap
+from .version import __version__
 
-__all__ = ['go', 'Sankey', 'Sunburst', 'Treemap']
+__all__ = ['go', 'Sankey', 'Sunburst', 'Treemap', '__version__']
